606b62c100c1c7102f4203cc941f1b8a08129dca
Hugo Clarke-Wing <7689302+clarkewing@users.noreply.github.com>
2024-10-09 20:49:05 +0200

    Restructure for HACS

---
+++ tests/const.py
@@ -0,0 +1,9 @@
+"""Constants for APM CrewConnect tests."""
+from custom_components.apm.const import (
+    CONF_PASSWORD,
+)
+from custom_components.apm.const import (
+    CONF_USERNAME,
+)
+
+MOCK_CONFIG = {CONF_USERNAME: "test_username", CONF_PASSWORD: "test_password"}
